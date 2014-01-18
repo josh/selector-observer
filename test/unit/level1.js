a366f78790bb0fd6a5face3d8630dca3712cc389
Joshua Peek <josh@joshpeek.com>
2014-01-18 02:35:59 -0600

    Test level1 contextual selectors

--- test/unit/level1.js
+++ test/unit/level1.js
@@ -52,8 +52,8 @@
     fixture.appendChild(h2);
   });
 
-  test('class selector', function() {
-    expect(2);
+  test('add element with class selector', function() {
+    expect(1);
 
     var fixture = document.getElementById('qunit-fixture');
     var observer = new SelectorObserver(fixture);
@@ -63,6 +63,41 @@
 
     observer.observe('H1.pastoral', function() {
       equal(this, h1);
+      start();
+    });
+    stop();
+
+    fixture.appendChild(h1);
+  });
+
+  test('add className with class selector', function() {
+    expect(1);
+
+    var fixture = document.getElementById('qunit-fixture');
+    var observer = new SelectorObserver(fixture);
+
+    var h1 = document.createElement('h1');
+
+    observer.observe('H1.pastoral', function() {
+      equal(this, h1);
+      start();
+    });
+    stop();
+
+    fixture.appendChild(h1);
+    h1.className = 'pastoral';
+  });
+
+  test('remove element with class selector', function() {
+    expect(1);
+
+    var fixture = document.getElementById('qunit-fixture');
+    var observer = new SelectorObserver(fixture);
+
+    var h1 = document.createElement('h1');
+    h1.className = 'pastoral';
+
+    observer.observe('H1.pastoral', function() {
       fixture.removeChild(h1);
 
       return function() {
@@ -75,8 +110,52 @@
     fixture.appendChild(h1);
   });
 
-  test('id selector', function() {
-    expect(2);
+  test('change className with class selector', function() {
+    expect(1);
+
+    var fixture = document.getElementById('qunit-fixture');
+    var observer = new SelectorObserver(fixture);
+
+    var h1 = document.createElement('h1');
+    h1.className = 'pastoral';
+
+    observer.observe('H1.pastoral', function() {
+      h1.className = 'sidenote';
+
+      return function() {
+        equal(h1, this);
+        start();
+      };
+    });
+    stop();
+
+    fixture.appendChild(h1);
+  });
+
+  test('change class attribute with class selector', function() {
+    expect(1);
+
+    var fixture = document.getElementById('qunit-fixture');
+    var observer = new SelectorObserver(fixture);
+
+    var h1 = document.createElement('h1');
+    h1.className = 'pastoral';
+
+    observer.observe('H1.pastoral', function() {
+      h1.setAttribute('class', 'sidenote');
+
+      return function() {
+        equal(h1, this);
+        start();
+      };
+    });
+    stop();
+
+    fixture.appendChild(h1);
+  });
+
+  test('add element with id selector', function() {
+    expect(1);
 
     var fixture = document.getElementById('qunit-fixture');
     var observer = new SelectorObserver(fixture);
@@ -90,6 +169,45 @@
 
     observer.observe('#z98y', function() {
       equal(this, p);
+      start();
+    });
+    stop();
+
+    fixture.appendChild(p);
+  });
+
+  test('change id with id selector', function() {
+    expect(1);
+
+    var fixture = document.getElementById('qunit-fixture');
+    var observer = new SelectorObserver(fixture);
+
+    var p = document.createElement('p');
+
+    observer.observe('H1#z98y', function() {
+      ok(false);
+    });
+
+    observer.observe('#z98y', function() {
+      equal(this, p);
+      start();
+    });
+    stop();
+
+    fixture.appendChild(p);
+    p.id = 'z98y';
+  });
+
+  test('remove element with id selector', function() {
+    expect(1);
+
+    var fixture = document.getElementById('qunit-fixture');
+    var observer = new SelectorObserver(fixture);
+
+    var p = document.createElement('p');
+    p.id = 'z98y';
+
+    observer.observe('#z98y', function() {
       fixture.removeChild(p);
 
       return function() {
@@ -100,5 +218,114 @@
     stop();
 
     fixture.appendChild(p);
+  });
+
+  test('change id with id selector', function() {
+    expect(1);
+
+    var fixture = document.getElementById('qunit-fixture');
+    var observer = new SelectorObserver(fixture);
+
+    var p = document.createElement('p');
+    p.id = 'z98y';
+
+    observer.observe('#z98y', function() {
+      p.id = 'z98x';
+
+      return function() {
+        equal(p, this);
+        start();
+      };
+    });
+    stop();
+
+    fixture.appendChild(p);
+  });
+
+  test('change id attribute with id selector', function() {
+    expect(1);
+
+    var fixture = document.getElementById('qunit-fixture');
+    var observer = new SelectorObserver(fixture);
+
+    var p = document.createElement('p');
+    p.id = 'z98y';
+
+    observer.observe('#z98y', function() {
+      p.setAttribute('id', 'z98x');
+
+      return function() {
+        equal(p, this);
+        start();
+      };
+    });
+    stop();
+
+    fixture.appendChild(p);
+  });
+
+  test('add element to parent with contextual selector', function() {
+    expect(1);
+
+    var fixture = document.getElementById('qunit-fixture');
+    var observer = new SelectorObserver(fixture);
+
+    var h1 = document.createElement('h1');
+    var em = document.createElement('em');
+
+    observer.observe('H1 EM', function() {
+      equal(this, em);
+      start();
+    });
+    stop();
+
+    fixture.appendChild(h1);
+    h1.appendChild(em);
+  });
+
+  test('remove child element from parent with contextual selector', function() {
+    expect(1);
+
+    var fixture = document.getElementById('qunit-fixture');
+    var observer = new SelectorObserver(fixture);
+
+    var h1 = document.createElement('h1');
+    var em = document.createElement('em');
+
+    observer.observe('H1 EM', function() {
+      h1.removeChild(em);
+
+      return function() {
+        equal(this, em);
+        start();
+      };
+    });
+    stop();
+
+    fixture.appendChild(h1);
+    h1.appendChild(em);
+  });
+
+  test('remove parent element with contextual selector', function() {
+    expect(1);
+
+    var fixture = document.getElementById('qunit-fixture');
+    var observer = new SelectorObserver(fixture);
+
+    var h1 = document.createElement('h1');
+    var em = document.createElement('em');
+
+    observer.observe('H1 EM', function() {
+      fixture.removeChild(h1);
+
+      return function() {
+        equal(this, em);
+        start();
+      };
+    });
+    stop();
+
+    fixture.appendChild(h1);
+    h1.appendChild(em);
   });
 })();
