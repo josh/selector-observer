a7546115229b4e8558e1248727053ac58f0bbe9c
Joshua Peek <josh@joshpeek.com>
2014-01-22 18:57:53 -0600

    Add more browsers to test

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,12 +68,14 @@ module.exports = function(grunt) {
           build: process.env.TRAVIS_JOB_ID,
           concurrency: 1,
           browsers: [
-            { browserName: 'safari', platform: 'OS X 10.9' },
-            { browserName: 'chrome', platform: 'Linux' },
-            { browserName: 'firefox', platform: 'Windows 8.1' },
+            { browserName: 'chrome', platform: 'Windows 8.1' },
+            { browserName: 'firefox', platform: 'Linux' },
             { browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
             { browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
-            { browserName: 'internet explorer', version: '9', platform: 'Windows 7' }
+            { browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
+            { browserName: 'safari', version: '7', platform: 'OS X 10.9' },
+            { browserName: 'safari', version: '6', platform: 'OS X 10.8' },
+            { browserName: 'safari', version: '5', platform: 'OS X 10.6' }
           ]
         }
       }
