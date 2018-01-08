d43ce0c93bf31f8db910bf05d808adfbc2f6efa2
Joshua Peek <josh@joshpeek.com>
2018-01-08 13:40:40 -0800

    Test twiddle classname

--- test/add-remove.js
+++ test/add-remove.js
@@ -67,4 +67,70 @@ suite('add/remove', function() {
 
     observer.abort()
   })
+
+  test('classname', async function() {
+    const className = randomClassName()
+    let addRan = false
+    let removeRan = false
+
+    const el = document.createElement('div')
+    body.appendChild(el)
+
+    const observer = observe(`.${className}`, {
+      add() {
+        addRan = true
+      },
+      remove() {
+        removeRan = true
+      }
+    })
+
+    el.className = className
+    await timeout(10)
+    assert.equal(addRan, true, 'add ran')
+    assert.equal(removeRan, false, 'remove ran')
+
+    el.className = ''
+    await timeout(10)
+    assert.equal(addRan, true, 'add ran')
+    assert.equal(removeRan, true, 'remove ran')
+
+    body.removeChild(el)
+    observer.abort()
+  })
+
+  test('classname multiple', async function() {
+    const className = randomClassName()
+    let addRan = 0
+    let removeRan = 0
+
+    const el = document.createElement('div')
+    body.appendChild(el)
+
+    const observer = observe(`.${className}`, {
+      add() {
+        addRan++
+      },
+      remove() {
+        removeRan++
+      }
+    })
+
+    let times = 1
+    while (times < 4) {
+      el.className = className
+      await timeout(10)
+      assert.equal(addRan, times, 'initialize ran')
+      assert.equal(removeRan, times - 1, 'initialize ran')
+
+      el.className = ''
+      await timeout(10)
+      assert.equal(removeRan, times, 'initialize ran')
+
+      times++
+    }
+
+    body.removeChild(el)
+    observer.abort()
+  })
 })
